c1d280856a6f7cbd9a3de14ceebd27921881c171
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2019-04-01 22:55:37 +0900

    Initial commit

---
+++ Assets/App/Scripts/BoardManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+    public float m_HorizontalStep;
+    public float m_VerticalStep;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
